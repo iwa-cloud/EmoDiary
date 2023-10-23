b1a9670c8cf85e2433904a1d272e270087ff6367
maki0817 <115522922+maki0817@users.noreply.github.com>
2023-10-23 11:29:04 +0900

    変更

--- 04_ソースコード/login.php
+++ 04_ソースコード/login.php
@@ -3,17 +3,10 @@
 <head>
     <meta content="text/html; charset=utf-8" http-equiv="Content-Type">
   <title>ログイン画面</title>
-  <style>
-    .a{
-        border: none;
-        outline: none;
-        border-bottom: 1px solid #999;
-        padding: 5px 10px;
-      }
-  </style>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 <meta name="viewport" content="width=device-width, initial-scale=1" />
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+<link rel="stylesheet" href="style.css">
 </head>
 <body>
 
