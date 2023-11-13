fbae926aa0013e3f54bec93aaf795a397ce0c2e2
ABE12202022 <115522918+ABE12202020@users.noreply.github.com>
2023-11-13 14:11:44 +0900

    コンフリクト解消しました

--- 04_ソースコード/data_input.php
+++ 04_ソースコード/data_input.php
@@ -91,30 +91,22 @@
                         </summary>
                         <script type="text/javascript">
                          const element = document.getElementById("first");
-                        //  const element_1 = document.getElementById("first1_1");
-                         const element_2 = document.getElementById("first1_2");
-                         const element_3 = document.getElementById("first2_1");
-                         const element_4 = document.getElementById("first2_2");
-                         const Button = document.getElementById("parent");
-                        Button.addEventListener("click",function(){
-                          if(element_2.classList.contains("visible")) {
-                            //   element_1.classList.remove("visible");
-                            //  element_1.classList.add("hidden");
-                              element_2.classList.remove("visible");
-                              element_2.classList.add("hidden");
-                              element_3.classList.remove("hidden");
-                              element_3.classList.add("visible");
-                              element_4.classList.remove("hidden");
-                              element_4.classList.add("visible");
-                            }else{
-                            //   element_1.classList.remove("hidden");
-                            //  element_1.classList.add("visible");
-                              element_2.classList.remove("hidden");
-                              element_2.classList.add("visible");
-                              element_3.classList.remove("visible");
-                              element_3.classList.add("hidden");
-                              element_4.classList.remove("visible");
-                              element_4.classList.add("hidden");
+                         const element_1 = document.getElementById("first_1");
+                         const element_2 = document.getElementById("first_2");
+                        //  const element_3 = document.getElementById("first_3");
+                            function removeExample(){
+					            element_1.remove();
+                                element_2.remove();
+                                // element_3.remove();
+                                // element.innerHTML+= '<i type="button" style="font-size:20px;">ログアウト</i>';
+                                element.innerHTML+='<table><tr>';
+                                element.innerHTML+='<td><button>ボタン</button></td>';
+                                element.innerHTML+='<td><button>ボタン</button></td>';
+                                element.innerHTML+='</tr></table>';
+                                element.innerHTML+= '<i type="button" style="font-size:20px;">ユーザー情報変更</i>';
+                                // element.innnerHTML +='<button name="button">クリックしてね</button>';
+                                // document.write('<button name="button">クリックしてね</button>');
+                                // document.write('</div>');
                             }
                         });
                         // function removeExample(){
