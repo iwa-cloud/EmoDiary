57b6de41c84438709212599e9c354dd32299fde4
Aso2101210 <122762782+Aso2101210@users.noreply.github.com>
2023-11-21 12:43:38 +0900

    変更

--- 04_ソースコード/top.php
+++ 04_ソースコード/top.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
-<html>
+<html lang="ja">
 <head>
-    <div class="container-fluid">
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.2/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="style.css">
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.2/font/bootstrap-icons.css">
+<link rel="stylesheet" href="style.css">
     <title>TOP画面</title>
     <style>
         .visible {
@@ -42,49 +43,34 @@
           float: right;
           margin-right: 20px;
         } */
-
+ 
         #parent {
           /* width:25px; */
           float: right;
         }
         .half{
-          float:  left;               
+          float:  left;              
           margin:  5px;
           padding:  10px;  
         }
+
     </style>
 </head>
 <body>
-
-  <nav class="a" aria-label="Sixth navbar example" style="background-color: white;">
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-md-10" href="#" style="color:#DCB3FC; font-size:40px">&emsp;EmoDiary</div>
-            <div class="col-md-2" style="text-align:right">
-              <details>
-                  <summary>
-                    <i type="button" class="bi bi-plus-square" style="font-size:40px;" onclick="location.href='.php'"></i>&emsp;
-                    <i type="button" class="bi bi-search" style="font-size:40px" onclick="location.href='.php'"></i>&emsp;
-                    <i type="button" class="bi bi-person-fill" style="font-size:40px"></i>
-                  </summary> -->
-                  <summary>
-                        <!-- //元のコード -->
-                      <!-- <div id="first">
-                        <i type="button" id="first_1" class="bi bi-plus-square" style="font-size:40px;" onclick="location.href='login.php'"></i>&emsp;
-                        <i type="button" id="first_2" class="bi bi-search" style="font-size:40px" onclick="location.href='login.php'"></i>&emsp;
-                        <i type="button" id="first_3" class="bi bi-person-fill" style="font-size:40px" onclick="removeExample()"></i> 
-                        </div> -->
-                        <!-- //クラスの切り替えテスト -->
-                        <div id="first" class="first">
-                          <i type="button" id="parent" class="bi bi-person-fill" style="font-size:25px;" ></i>
-                          <i type="button" id="first1_2" class="visible bi bi-search" style="font-size:25px;" onclick="location.href='search.php'"></i>&emsp;
-                        <i type="button" id="first1_1" class="visible bi bi-plus-square" style="font-size:25px;" onclick="location.href='login.php'"></i>&emsp;
-                        <button type="button" id="first2_2" class="hidden" onclick="location.href='logout.php'">ログアウト</button>
-                        <button type="button" id="first2_1" class="hidden" onclick="location.href='usr_inf_chg_input.php'">ユーザー変更画面</button>
-                        <!-- <i type="button" id="first2_2" class="hidden" style="font-size:25px;" onclick="location.href='login.php'"></i>&emsp;                   
-                        <i type="button" id="first2_1" class="hidden" style="font-size:25px;" onclick="location.href='login.php'"></i>&emsp; -->
-
-                        </div>
+    <nav class="a" aria-label="Sixth navbar example" style= "background-color: white;">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-10"><i type="button" class="bi bi-chevron-left" style="font-size:40px;"></i>&emsp; <href="#" style="color:#DCB3FC; font-size:2.5vw;">&emsp;EmoDiary</div>
+                <div class="col-md-2" style="text-align:right">
+                    <details>
+                        <summary>
+                          <div id="first" class="first">
+                            <i type="button" id="parent" class="bi bi-person-fill" style="font-size:25px;" ></i>
+                            <i type="button" id="first1_2" class="visible bi bi-search" style="font-size:25px;" onclick="location.href='search.php'"></i>&emsp;
+                          <i type="button" id="first1_1" class="visible bi bi-plus-square" style="font-size:25px;" onclick="location.href='login.php'"></i>&emsp;
+                          <button type="button" id="first2_2" class="hidden" onclick="location.href='logout.php'">ログアウト</button>
+                          <button type="button" id="first2_1" class="hidden" onclick="location.href='usr_inf_chg_input.php'">ユーザー変更画面</button>
+                            </div>
                         </summary>
                         <script type="text/javascript">
                          const element = document.getElementById("first");
@@ -114,24 +100,6 @@
                               element_4.classList.add("hidden");
                             }
                         });
-                        // function removeExample(){
-					              //           element_1.remove();
-                        //           element_2.remove();
-                        //           element_3.remove();
-                        //           element.innerHTML= '<button id="first_1">ログアウト</button>';
-                        //           element.innerHTML= '<button id="first_2">ユーザー情報変更</button>';
-                        //           element.innerHTML= '<i type="button" id="first_3" class="bi bi-person-fill" style="font-size:20px;" onclick="restore()"></i>';
-                                
-                        //  }
-
-                        //  function restore(){
-                        //           element_1.remove();
-                        //           element_2.remove();
-                        //           element_3.remove();
-                        //           element.innerHTML= '<i type="button" id="first_1" class="bi bi-plus-square" style="font-size:40px;" onclick="location.href='login.php'"></i>&emsp;';
-                        //           element.innerHTML= '<i type="button" id="first_2" class="bi bi-search" style="font-size:40px" onclick="location.href='login.php'"></i>&emsp;';
-                        //           element.innerHTML= '<i type="button" id="first_3" class="bi bi-person-fill" style="font-size:40px" onclick="removeExample()"></i> ';
-                        //  }
                         </script>
               </details>
                         </div>
@@ -140,8 +108,8 @@
     </div>
   </nav>
   <div class="controller">
-  <div class="line">
-</div>
+    <div class="line" style="z-index: 1;">
+    </div>
 </div>  
   <div id="nav">
     <li><a href="#">最新 ↓</a></li>
@@ -153,13 +121,51 @@
   </div>
 </div>
 </div>
-<p class="kk" style="color:#DCB3FC">文章</p>
+<div class="container-fluid">
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <!-- <br>
+  <br>
+  <br> -->
+  <div style="margin-bottom: 5vh;">
+  <div class="row">
+      <div class="col-md-5"></div>
+      <div class="col-md-7">
+      <p class="kk" style="color:#DCB3FC">文章</p>
+      </div>
+      <div class="col-md-5"></div>
+      <div class="col-md-6">
+<form method="post" action="sample.cgi" class="h">
+  <textarea  class="e" name="kansou" style="min-height: 17vw; max-height: 17vw; min-width: 50vw; max-width: 50vw;"></textarea><br>
+  </form>
+      </div>
+      <div class="col-md-1"></div>
+      <div class="col-md-5"></div>
+      <div align="right" class="col-md-6">
+          <button class="form-control" style="color:#DCB3FC; width: 10vw;"onclick="location.href='login.php'">共有</button>
+      </div>
+      <div class="col-md-1"></div>
+  </div>
+</div>
+  </div>
+<!-- <p class="kk" style="color:#DCB3FC">文章</p>
 <form method="post" action="sample.cgi" class="h">
   <textarea name="kansou" rows="10" cols="100"></textarea><br>
   <div class="ky">
-  <button class="form-control" style="color:#DCB3FC; width: 100px; "onclick="location.href='login.php'">共有</button>
+  <button class="form-control" style="color:#DCB3FC; width: 10vw; "onclick="location.href='login.php'">共有</button>
   </div>
-  </form>
-</body>
+  </form> -->
+  <script src="./header.js">
+  </script>
 </body>
 </html>
+
